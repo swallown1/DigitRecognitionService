0b562b732cec04d907a6ccfcf8d634bfb936fc7f
ubuntu <supeng842499467@gmail.com>
2020-07-30 16:54:39 +0800

    flaskservice

--- flaskService/main.py
+++ flaskService/main.py
@@ -6,12 +6,13 @@
 # Date:         2020/7/30
 '''
 from PIL import Image
+import base64
 
 import numpy as np
 
 from keras.models import load_model
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 model = load_model('model.h5')
 
@@ -22,23 +23,37 @@ def data_process(data):
     return data
 
 app = Flask(__name__)
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict_data():
-    image = request.files.get('file')
+    t = request.form['file']
+    print(t)
+    t = t[23:]
+    print(t)
+    print(t)
+    image = base64.b64decode(t)
+    path = "test.jpeg"
+    file = open(path, 'wb')
+    file.write(image)
+    file.close()
+    # image = request.files.get('file')
+    # print(image)
     # file_path = image.filename
     # image.save(file_path)
 
     # read image and convert to gray
     # data = Image.open(file_path).convert('L')
-    data = Image.open(image).convert('L')
+    data = Image.open(path).convert('L')
     data = data.resize((28,28))
     data = np.array(data, 'f')
 
     data = data_process(data)
     result = model.predict(data)
-    print("res", result)
+    # print("res", result)
     return str(result.argmax())
 
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=5000)
+    app.run("0.0.0.0", port=5001)
