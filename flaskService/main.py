99dd9e184322c4c943ab3ea13f73353344b54666
ubuntu <supeng842499467@gmail.com>
2020-07-30 11:13:58 +0800

    flaskService

--- flaskService/main.py
+++ flaskService/main.py
@@ -5,10 +5,11 @@
 # Author:       neu
 # Date:         2020/7/30
 '''
+from PIL import Image
+
 import numpy as np
 
 from keras.models import load_model
-from test import *
 
 from flask import Flask, request
 
@@ -17,23 +18,27 @@ model = load_model('model.h5')
 def data_process(data):
     data = np.array(data)
     data = data.reshape((1, 28, 28, 1))
-    # data = data.astype('float64')/255
+    data = data.astype('float64')/255
     return data
 
 app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def predict_data():
-    # input = request.args.get('input')
-    (x_train, y_train), (x_test, y_test) = load_data()
-    data, label = generate_data(x_test, y_test)
+    image = request.files.get('file')
+    # file_path = image.filename
+    # image.save(file_path)
+
+    # read image and convert to gray
+    # data = Image.open(file_path).convert('L')
+    data = Image.open(image).convert('L')
+    data = data.resize((28,28))
+    data = np.array(data, 'f')
+
     data = data_process(data)
     result = model.predict(data)
     print("res", result)
-    print(result.argmax())
-    print(label)
-    return str(result)
+    return str(result.argmax())
 
 
 if __name__ == '__main__':
-    app.config['JSON_AS_ASCII'] = False
     app.run()
